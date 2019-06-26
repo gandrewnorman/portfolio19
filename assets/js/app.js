b0d5c476ecc8364e137e271701b94492b4d2e366
gandrewnorman <gandrewnorman@gmail.com>
2019-06-25 18:10:47 -0700

    First commit

---
+++ assets/js/app.js
@@ -0,0 +1,204 @@
+/* ========================================================================
+ * DOM-based Routing
+ * Based on http://goo.gl/EUTi53 by Paul Irish
+ * ======================================================================== */
+( function( $ ) {
+
+    var main = {
+        common: {
+            init: function() {
+                // Run everywhere.
+                $( '.menu, .overlay' ).click( function() {
+                    $( '.menu' ).toggleClass( 'clicked' );
+                    $( '#nav' ).toggleClass( 'show' );
+                } );
+
+                /* HIDE HEADER ON SCROLL DOWN  ******************************************************************************/
+                var didScroll;
+                var lastScrollTop = 0;
+                var delta = 5;
+                var navbarHeight = $( 'a.logo' ).outerHeight();
+
+                $( window ).scroll( function( event ) {
+                    didScroll = true;
+                } );
+
+                setInterval( function() {
+                    if ( didScroll ) {
+                        hasScrolled();
+                        didScroll = false;
+                    }
+                }, 250 );
+
+                function hasScrolled() {
+                    var st = $( this ).scrollTop();
+
+                    // Make sure they scroll more than delta
+                    if ( Math.abs( lastScrollTop - st ) <= delta )
+                        return;
+                    // If they scrolled down and are past the navbar, add class .nav-up.
+                    // This is necessary so you never see what is "behind" the navbar.
+                    if ( st > lastScrollTop && st > navbarHeight ) {
+                        // Scroll Down
+                        $( 'a.logo' ).removeClass( 'nav-down' ).addClass( 'nav-up' );
+                        console.log( 'nav up' );
+                    } else {
+                        // Scroll Up
+                        if ( st + $( window ).height() < $( document ).height() ) {
+                            $( 'a.logo' ).removeClass( 'nav-up' ).addClass( 'nav-down' );
+                            // $( '.menu-helper' ).addClass( 'hidden' );
+                            console.log( 'nav down' );
+                        }
+                    }
+
+                    lastScrollTop = st;
+                }
+
+                console.log( 'common fired' );
+            },
+            finalize: function() {
+                // Run everywhere. Loaded last.
+                // $( window ).on( "scroll", function() {
+                //     if ( $( window ).scrollTop() ) {
+                //         $( 'nav' ).addClass( 'black' );
+                //         console.log( 'scroll' );
+                //     } else {
+                //         $( 'nav' ).removeClass( 'black' );
+                //     }
+                // } );
+                console.log( 'loaded last fired' );
+            }
+        },
+        home: {
+            init: function() {
+                // Run on page with body class `home`.
+                /*CANVAS RAINBOX CODE **********************************************************
+                 *******************************************************************************************/
+                var canvas = document.getElementById( 'rainbox' );
+                var ctx = canvas.getContext( '2d' );
+                //making the canvas full screen
+                rainbox.height = window.innerHeight;
+                rainbox.width = window.innerWidth;
+                //creating string for drops
+                var emojidrops = "🎬 🎥 🎵 💧 ⚡ 🌴";
+                //converting the string into an array of single characters
+                emojidrops = emojidrops.split( " " );
+                var font_size = 16;
+                var columns = rainbox.width / font_size; //number of columns for the rain
+                //an array of drops - one per column
+                var drops = [];
+                //x below is the x coordinate
+                //1 = y co-ordinate of the drop(same for every drop initially)
+                for ( var x = 0; x < columns; x++ )
+                    drops[ x ] = 1;
+                //drawing the characters
+                function draw() {
+                    //translucent BG to show trail
+                    ctx.fillStyle = "rgba(17, 17, 17, 0.25)";
+                    ctx.fillRect( 0, 0, rainbox.width, rainbox.height );
+                    ctx.fillStyle = "#ede9e3"; //@ivory text
+                    ctx.font = font_size + "px brandon-grotesque";
+                    //looping over drops
+                    for ( var i = 0; i < drops.length; i++ ) {
+                        //a random emojidrops character to print
+                        var text = emojidrops[ Math.floor( Math.random() * emojidrops.length ) ];
+                        //x = i*font_size, y = value of drops[i]*font_size
+                        ctx.fillText( text, i * font_size, drops[ i ] * font_size );
+                        //sending the drop back to the top randomly after it has crossed the screen
+                        //adding a randomness to the reset to make the drops scattered on the Y axis
+                        if ( drops[ i ] * font_size > rainbox.height && Math.random() > 0.989 )
+                            drops[ i ] = 0;
+                        //incrementing Y coordinate
+                        drops[ i ]++;
+                    }
+                }
+                setInterval( draw, 66 );
+
+                console.log( 'home body fired' );
+            }
+        },
+        tcs: {
+            init: function() {
+                // Run on page with body class `tcs`
+                /* MAGNIFIC POPUP CODE **********************************************************
+                 *******************************************************************************************/
+                $( '.portfolio-box' ).magnificPopup( {
+                    type: 'iframe',
+                    iframe: {
+                        patterns: {
+                            youtube: {
+                                index: 'youtube.com/',
+                                id: function( url ) {
+                                    var m = url.match( /[\\?\\&]v=([^\\?\\&]+)/ );
+                                    if ( !m || !m[ 1 ] ) return null;
+                                    return m[ 1 ];
+                                },
+                                src: '//www.youtube.com/embed/%id%?autoplay=1&rel=0'
+                            },
+                            vimeo: {
+                                index: 'vimeo.com/',
+                                id: function( url ) {
+                                    var m = url.match( /(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/ );
+                                    if ( !m || !m[ 5 ] ) return null;
+                                    return m[ 5 ];
+                                },
+                                src: '//player.vimeo.com/video/%id%?autoplay=1'
+                            }
+                        }
+                    }
+                } );
+
+                // IG Carousel
+                $('#carouselExample').on('slide.bs.carousel', function(e) {
+
+                var $e = $(e.relatedTarget);
+                var idx = $e.index();
+                var itemsPerSlide = 8;
+                var totalItems = $('.carousel-item').length;
+
+                if (idx >= totalItems - (itemsPerSlide - 1)) {
+                    var it = itemsPerSlide - (totalItems - idx);
+                    for (var i = 0; i < it; i++) {
+                        // append slides to end
+                        if (e.direction == "left") {
+                            $('.carousel-item').eq(i).appendTo('.carousel-inner');
+                        } else {
+                            $('.carousel-item').eq(0).appendTo('.carousel-inner');
+                        }
+                    }
+                }
+            });
+
+            $('#carouselExample').carousel({
+                // interval: 2000
+            });
+
+                console.log( 'code-snippets fired' );
+            }
+        }
+    };
+
+    var util = {
+        fire: function( func, funcname, args ) {
+            var namespace = main;
+            funcname = ( funcname === undefined ) ? 'init' : funcname;
+            if ( func !== '' && namespace[ func ] && typeof namespace[ func ][ funcname ] === 'function' ) {
+                namespace[ func ][ funcname ]( args );
+            }
+        },
+        loadEvents: function() {
+
+            util.fire( 'common' );
+
+            $.each( document.body.className.replace( /-/g, '_' ).split( /\s+/ ), function( i, classnm ) {
+                util.fire( classnm );
+            } );
+
+
+            util.fire( 'common', 'finalize' );
+        }
+    };
+
+    $( document ).ready( util.loadEvents );
+
+} )( jQuery );
